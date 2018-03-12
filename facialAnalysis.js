437cdb80c811156138a9f37efc6de83a0858ee2c
Vignesh Sankaran <developer@sankaran.io>
2018-03-12 21:32:41 +1100

    Work in progress

---
+++ facialAnalysis.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const rek = new AWS.Rekognition();
+
+// Offer different endpoints available for the handler to access
+class facialAnalysis {
+
+}
---
+++ lambdaHandler.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const FacialAnalysis = require("./facialAnalysis.js");
+
+module.exports.facialAnalysis = (event, context, callback) => {
+    
+}
