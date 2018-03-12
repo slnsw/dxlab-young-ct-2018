a24d124efb53c4e71969c184392d89bf2f919ffe
Vignesh Sankaran <developer@sankaran.io>
2018-03-12 21:32:41 +1100

    Rudimentary facial recognition endpoint now functional

---
+++ facialAnalysis.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const rek = new AWS.Rekognition();
+
+// Offer different endpoints available for the handler to access
+class FacialAnalysis {
+    static listFaces() {
+        const s3Params = {
+            Bucket: "samhood",
+            MaxKeys: 3
+        };
+
+        return new Promise((resolve, reject) => {
+            s3.listObjects(s3Params, function(error, s3Data) {
+                if (error) {
+                    return reject(new Error(error));
+                }
+                if (s3Data.length === 0) {
+                    return reject(new Error("There are no images in the s3 bucket"));
+                }
+
+                var imageData = [];
+
+                s3Data.Contents.forEach(function(item, i) {
+                    const rekParams = {
+                        Image: {
+                            S3Object: {
+                                Bucket: s3Params.Bucket,
+                                Name: item.Key
+                            }
+                        }
+                    };
+
+                    rek.detectFaces(rekParams, function(rekError, rekData) {
+                        if (rekError) {
+                            return reject(new Error(rekError));
+                        }
+                        imageData.push({[item.Key]: rekData});
+
+                        if (i === s3Data.MaxKeys - 1) {
+                            console.log(rekData);
+                            return resolve(imageData);
+                        }
+                    });
+
+                });
+            });
+        });
+    }
+}
+
+module.exports = FacialAnalysis;
