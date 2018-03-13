e2c71ab74f8693847a36fe98dff0f64b34457fe6
Vignesh Sankaran <developer@sankaran.io>
2018-03-14 09:31:29 +1100

    WIP

--- lambdaHandler.js
+++ lambdaHandler.js
@@ -3,5 +3,19 @@
 const FacialAnalysis = require("./facialAnalysis.js");
 
 module.exports.facialAnalysis = (event, context, callback) => {
-    
+    // Provide an endpoint to the facial analysis endpiont
+    return FacialAnalysis.listFaces().then((data) => {
+        const response = {
+            isBase64Encoded: false,
+            statusCode: 200,
+            body: JSON.stringify(data)
+        }
+    })
+    .catch((error) => {
+        callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: error.message || 'Internal server error',
+        })
+    });
 }
