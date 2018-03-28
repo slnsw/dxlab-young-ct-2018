e5ae7a2ede0bce04ba377cece76e989b771ace59
Vignesh Sankaran <developer@sankaran.io>
2018-03-28 18:50:25 +1100

    Restructuring codebase to better accommodate testing

--- iat-back/__tests__/rekognition.test.js
+++ iat-back/__tests__/rekognition.test.js
@@ -1,9 +1,1 @@
-import {promisify} from "util";
-import rekognition from "../rekognition";
-
-test(`No params return 400`, () => {
-    const result = handler({}, {});
-    result.then(data => {
-        
-    })
-});
+'use strict';
