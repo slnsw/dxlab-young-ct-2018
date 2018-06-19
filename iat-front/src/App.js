3e02b51d987d9a52e5a380d5bee8efa3d54e7a9b
Vignesh Sankaran <developer@sankaran.io>
2018-06-19 10:31:55 +1000

    Forward and back buttons are now functional

--- iat-front/src/App.js
+++ iat-front/src/App.js
@@ -6,12 +6,12 @@ import "./App.css";
 import "./gallery.css";
 
 class App extends Component {
-  state = { images: [] };
+  state = { imageList: [] };
   async componentDidMount() {
     const { data } = await axios.get(
       "https://oc958ljit3.execute-api.ap-southeast-2.amazonaws.com/dev/images"
     );
-    this.setState({ images: data.body });
+    this.setState({ imageList: data.body });
   }
   render() {
     const imageName = window.location.search.replace("?image=", "");
@@ -19,9 +19,9 @@ class App extends Component {
     return (
       <div className="App">
         {imageName ? (
-          <Image imageName={imageName} />
+          <Image imageList={this.state.imageList} imageName={imageName} />
         ) : (
-          this.state.images.map(image => {
+          this.state.imageList.map(image => {
             return (
               <a href={`?image=${image}`} key={image}>
                 <img
--- iat-front/src/Image.js
+++ iat-front/src/Image.js
@@ -11,11 +11,35 @@ class Image extends Component {
     console.log(data.body);
   }
   render() {
+    const currentIndex = this.props.imageList.indexOf(this.props.imageName);
+    console.log(currentIndex);
     return (
       <React.Fragment>
         <h1>{this.props.imageName}</h1>
         <p>
-          <a href="http://localhost:3000">Return to index page</a>
+          <a href="/">Return to index page</a>
+        </p>
+        <p>
+          <a
+            href={`?image=${
+              currentIndex === 0
+                ? this.props.imageList[this.props.imageList.length - 1]
+                : this.props.imageList[currentIndex - 1]
+            }`}
+          >
+            Back
+          </a>
+        </p>
+        <p>
+          <a
+            href={`?image=${
+              currentIndex === this.props.imageList.length - 1
+                ? this.props.imageList[0]
+                : this.props.imageList[currentIndex + 1]
+            }`}
+          >
+            Next
+          </a>
         </p>
         <img
           src={`https://s3-ap-southeast-2.amazonaws.com/samhood/${
